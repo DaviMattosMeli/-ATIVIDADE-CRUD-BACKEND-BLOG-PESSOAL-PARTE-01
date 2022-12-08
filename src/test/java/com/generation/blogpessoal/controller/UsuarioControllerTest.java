be63a46c4e72d891bf34ae63e733b0282a688cd2
DaviMattosMeli <davi.mattos@mercadolivre.com>
2022-12-08 17:53:12 -0300

    Teste atualizarUsuario() feito

--- src/test/java/com/generation/blogpessoal/controller/UsuarioControllerTest.java
+++ src/test/java/com/generation/blogpessoal/controller/UsuarioControllerTest.java
@@ -82,6 +82,27 @@ public class UsuarioControllerTest {
         assertEquals(HttpStatus.OK, corpoResposta.getStatusCode());
     }
 
+    @Test
+    @DisplayName("Atualizar um Usuário")
+    public void deveAtualizarUmUsuario() {
+
+        Optional<Usuario> usuarioCadastrado = usuarioService.cadastrarUsuario(new Usuario(0L,
+                "Juliana Andrews", "juliana_andrews@email.com.br", "juliana123", "https://i.imgur.com/yDRVeK7.jpg"));
+
+        Usuario usuarioUpdate = new Usuario(usuarioCadastrado.get().getId(),
+                "Juliana Andrews Ramos", "juliana_ramos@email.com.br", "juliana123" , "https://i.imgur.com/yDRVeK7.jpg");
+
+        HttpEntity<Usuario> corpoRequisicao = new HttpEntity<Usuario>(usuarioUpdate);
+
+        ResponseEntity<Usuario> corpoResposta = testRestTemplate
+                .withBasicAuth("root@root.com", "rootroot")
+                .exchange("/usuarios/atualizar", HttpMethod.PUT, corpoRequisicao, Usuario.class);
+
+        assertEquals(HttpStatus.OK, corpoResposta.getStatusCode());
+        assertEquals(corpoRequisicao.getBody().getNome(), corpoResposta.getBody().getNome());
+        assertEquals(corpoRequisicao.getBody().getUsuario(), corpoResposta.getBody().getUsuario());
+    }
+
 
     @Test
     @DisplayName("Não deve permitir duplicação do Usuário")
@@ -90,7 +111,7 @@ public class UsuarioControllerTest {
         usuarioService.cadastrarUsuario(new Usuario(0L,
                 "Maria da Silva", "maria_silva@email.com.br", "13465278", "https://i.imgur.com/T12NIp9.jpg"));
 
-        HttpEntity<Usuario> corpoRequisicao = new HttpEntity<Usuario>(new Usuario(0L,
+        HttpEntity<Usuario> corpoRequisicao = new HttpEntity<>(new Usuario(0L,
                 "Maria da Silva", "maria_silva@email.com.br", "13465278", "https://i.imgur.com/T12NIp9.jpg"));
 
         ResponseEntity<Usuario> corpoResposta = testRestTemplate
